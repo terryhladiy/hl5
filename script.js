1e50a2011c4eac4ae026385f727507e6642c4472
taras <tarik2002sta@gmail/com>
2022-12-11 19:33:55 +0200

    firct commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const firstWord = prompt('Введіть перше слово');
+const secondWord = prompt('Введіть друге слово');
+const thirdWord = prompt('Ведіть третє слово');
+
+
+const resultString = firstWord + ', ' + secondWord + ', ' + thirdWord + '.';
+
+alert(resultString);
+
+const number = prompt('Введіть п\'ятизначне число',0);
+alert(`${number[0]} ${number[1]} ${number[2]} ${number[3]} ${number[4]}`);
+
+
+
+
+
+
+/* 
+const number = prompt('Введіть п\'ятизначне число',0);  
+if (isNaN(number) === true) {  
+alert('Ви дуже не уважні😤');  
+} else {   
+alert(`${number[0]} ${number[1]} ${number[2]} ${number[3]} ${number[4]}`);  
+} 
+console.log(number);
+
+
+ */
